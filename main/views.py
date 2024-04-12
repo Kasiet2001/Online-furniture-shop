1187769f60a13a3672b3977afc06097ba7402197
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-04-12 14:53:25 +0600

    Initial commit

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from goods.models import Categories
+
+def index(request):
+    context = {
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
+
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Текст о том почему этот магазин такой классный, и какой хороший товар.'
+    }
+    return render(request, 'main/about.html', context)
