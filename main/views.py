a7d1931235fc6faee252621fba558630d3cfc6e5
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-06-27 19:05:23 +0600

    class based views

--- main/views.py
+++ main/views.py
@@ -1,19 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from goods.models import Categories
+from django.views.generic import TemplateView
 
-def index(request):
-    context = {
-        'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME',
 
-    }
-    return render(request, 'main/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
 
-def about(request):
-    context = {
-        'title': 'Home - О нас',
-        'content': 'О нас',
-        'text_on_page': 'Текст о том почему этот магазин такой классный, и какой хороший товар.'
-    }
-    return render(request, 'main/about.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Главная'
+        context['content'] = 'Магазин мебели HOME'
+        return context
+
+# def index(request):
+#     context = {
+#         'title': 'Home - Главная',
+#         'content': 'Магазин мебели HOME',
+#
+#     }
+#     return render(request, 'main/index.html', context)
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - О нас'
+        context['content'] = 'Home - О нас'
+        context['text_on_page'] = 'Текст о том почему этот магазин такой классный, и какой хороший товар.'
+        return context
+
+# def about(request):
+#     context = {
+#         'title': 'Home - О нас',
+#         'content': 'О нас',
+#         'text_on_page': 'Текст о том почему этот магазин такой классный, и какой хороший товар.'
+#     }
+#     return render(request, 'main/about.html', context)
